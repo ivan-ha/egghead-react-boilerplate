2d098decd865150a95c87a14ffe108539c6ae302
Ivan Ha <hangolam@gmail.com>
2019-01-19 13:28:53 +0800

    chore: add support to class properties syntax

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React from "react";
 
 class App extends React.Component {
+  state = {
+    count: 0
+  };
+
+  plusClick = () => this.setState(({ count }) => ({ count: count + 1 }));
+
+  minusClick = () => this.setState(({ count }) => ({ count: count - 1 }));
+
   render() {
-    return <h1>Hello World</h1>;
+    return (
+      <React.Fragment>
+        <h1>Hello World</h1>
+        <h2>{this.state.count}</h2>
+        <button onClick={this.plusClick}>+</button>
+        <button onClick={this.minusClick}>-</button>
+      </React.Fragment>
+    );
   }
 }
 
